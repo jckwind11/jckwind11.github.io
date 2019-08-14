4e46260f6bc28f31e5ea6841e17f79bb5245d333
Jack Windham <5225ancara@gmail.com>
2019-08-14 01:25:39 -0400

    format

--- assets/javascript/verify.js
+++ assets/javascript/verify.js
@@ -1,6 +1,8 @@
 const searchData = new URLSearchParams(window.location.search);
 const token = searchData.get('token');
-const xhttp = new XMLHttpRequest();
-xhttp.open("POST", "https://dev.cordia.app/v1/user/verify", true);
-xhttp.setRequestHeader("Token", token);
-xhttp.send();
+if (token) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "https://dev.cordia.app/v1/user/verify", true);
+    xhttp.setRequestHeader("Token", token);
+    xhttp.send();
+}
