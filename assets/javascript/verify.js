c6ae61565206c4899e9bc339aec4a78c463a7cf2
Jack Windham <5225ancara@gmail.com>
2019-08-14 10:38:08 -0400

    fixed html

--- assets/javascript/verify.js
+++ assets/javascript/verify.js
@@ -1,14 +1,21 @@
 const searchData = new URLSearchParams(window.location.search);
 const token = searchData.get('token');
-const headers = { headers: { 'Token' : token } };
+const headers = {
+    headers: {
+        'Token': token,
+        'Access-Control-Allow-Origin': "*",
+        'Access-Control-Allow-Methods': "POST",
+        'Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    }
+};
 const body = {};
 const url = `https://dev.cordia.app/v1/user/verify`;
 axios.post(url, body, headers)
-.then(function (response) {
-    // handle success
-    console.log(response);
-})
-.catch(function (error) {
-    alert(error);
-    console.log(error);
-})
+    .then(function (response) {
+        // handle success
+        console.log(response);
+    })
+    .catch(function (error) {
+        alert(error);
+        console.log(error);
+    })
