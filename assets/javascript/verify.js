e452458332c777bad28fc6437831f8c271451bc7
Jack Windham <5225ancara@gmail.com>
2019-08-14 13:51:48 -0400

    added async

--- assets/javascript/verify.js
+++ assets/javascript/verify.js
@@ -1,14 +1,19 @@
 const searchData = new URLSearchParams(window.location.search);
 const token = searchData.get('token');
-const headers = { headers: { 'Token': token } };
-const body = JSON.stringify({});
-const url = `https://dev.cordia.app/v1/user/verify`;
-axios.post(url, body, headers)
-    .then(function (response) {
-        alert('You have been verified!')
-        console.log(response);
-    })
-    .catch(function (error) {
+main(token)
+
+async function main(token) {
+    try {
+        await verifyUser(token)
+        window.location.replace("https://google.com");
+    } catch (error) {
         alert(error);
-        console.log(error);
-    })
+    }
+}
+
+async function verifyUser(token) {
+    const headers = { headers: { 'Token': token } };
+    const body = JSON.stringify({});
+    const url = 'https://dev.cordia.app/v1/user/verify';
+    return axios.post(url, body, headers);
+}
