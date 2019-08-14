c973343a4c3ca5c94a6c893b3b900ff5f22bdbf4
Jack Windham <5225ancara@gmail.com>
2019-08-14 01:30:45 -0400

    should alert status

--- assets/javascript/verify.js
+++ assets/javascript/verify.js
@@ -2,7 +2,10 @@ const searchData = new URLSearchParams(window.location.search);
 const token = searchData.get('token');
 if (token) {
     const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        alert(this.status);
+    };
     xhttp.open("POST", "https://dev.cordia.app/v1/user/verify", true);
     xhttp.setRequestHeader("Token", token);
     xhttp.send();
-}
+};
