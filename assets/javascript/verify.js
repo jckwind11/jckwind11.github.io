f0701edcc5a9e34cfc8030c2959025236e614eb5
Jack Windham <5225ancara@gmail.com>
2019-08-14 12:09:41 -0400

    alerts when success

--- assets/javascript/verify.js
+++ assets/javascript/verify.js
@@ -1,15 +1,11 @@
 const searchData = new URLSearchParams(window.location.search);
 const token = searchData.get('token');
-const headers = {
-    headers: {
-        'Token': token
-    }
-};
-const body = JSON.stringify({"test": "hehe"});
+const headers = { headers: { 'Token': token } };
+const body = JSON.stringify({});
 const url = `https://dev.cordia.app/v1/user/verify`;
 axios.post(url, body, headers)
     .then(function (response) {
-        // handle success
+        alert('You have been verified!')
         console.log(response);
     })
     .catch(function (error) {
