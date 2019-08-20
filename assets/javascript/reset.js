d9d089d7691380f313d8f1d3ed4f971e8ea480a7
Jack Windham <5225ancara@gmail.com>
2019-08-19 21:04:44 -0400

    test1

--- assets/javascript/reset.js
+++ assets/javascript/reset.js
@@ -4,19 +4,23 @@ document.addEventListener('DOMContentLoaded', function () {
     // Get the one-time code from the query parameter.
     const searchData = new URLSearchParams(window.location.search);
     const actionCode = searchData.get('oobCode');
+    const apiKey = searchData.get('apiKey');
 
-    // Configure the Firebase SDK.
-    // This is the minimum configuration required for the API to be used.
-    const config = { 'apiKey': "AIzaSyBVFrJiXqMr4H0n3W0TFD3Y8JBqxdB_Akc" };
-    var app = firebase.initializeApp(config);
-    var auth = app.auth();
+    if (apiKey) {
+        const config = { 'apiKey': apiKey };
+        const app = firebase.initializeApp(config);
+        const auth = app.auth();
 
-    // Handle the user management action.
-    if (actionCode) {
-        handleResetPassword(auth, actionCode)
+        // Handle the user management action.
+        if (actionCode) {
+            handleResetPassword(auth, actionCode)
+        } else {
+            document.getElementById("statusLabel").innerHTML = "Invalid token";
+            document.getElementById("reasonLabel").innerHTML = "Could not find verification token";
+        }
     } else {
-        document.getElementById("statusLabel").innerHTML = "Invalid token";
-        document.getElementById("reasonLabel").innerHTML = "Could not find verification token";
+        document.getElementById("statusLabel").innerHTML = "Connectivity issue";
+        document.getElementById("reasonLabel").innerHTML = "Could not securely connect to our servers";
     }
 }, false);
 
@@ -33,8 +37,8 @@ function handleResetPassword(auth, actionCode) {
                 "Either your password was too weak or your verification token is invalid or expired. Please try to reset the password again", false);
         });
     }).catch(function (error) {
-        updateUI("Invalid verification token",
-            "Your verification token is invalid or expired. Please try to reset the password again", false);
+        // "Your verification token is invalid or expired. Please try to reset the password again"
+        updateUI("Invalid verification token", error, false);
     });
 }
 
